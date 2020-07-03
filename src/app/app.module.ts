2c2d70448545f5f5a00c678475aedc2099c379ea
vasanna.nguyen <vasanna.nguyen@accenture.com>
2020-07-03 14:49:05 -0500

    building base interface before building services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
     imports: [
       BrowserModule,
       ReactiveFormsModule,
+      HttpClientModule,
+      RouterModule.forRoot([
+          { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+          { path: 'dashboard', component: DashboardComponent }
+      ])
     ],
     declarations: [
       AppComponent,
-      TopBarComponent
+      TopBarComponent,
+      DashboardComponent
+      
     ],
     bootstrap: [ AppComponent ]
   })
